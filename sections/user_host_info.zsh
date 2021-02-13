5d0713624bfe40d4f801c754ed8e73295df86a88
Robert David <robert.david.public@gmail.com>
2021-02-13 22:50:55 +0100

    sections/user_host_info.zsh: remove default user and print local root

--- sections/user_host_info.zsh
+++ sections/user_host_info.zsh
@@ -1,14 +1,14 @@
-slimline::section::user_host_info::init() {
-  slimline_section_user_host_info_default_user="${SLIMLINE_USER_HOST_INFO_DEFAULT_USER:-${USER}}"
-}
-
 slimline::section::user_host_info::render() {
   if (( ! ${SLIMLINE_ALWAYS_SHOW_USER_HOST_INFO:-0} )) && \
-      [[ -z "${SSH_TTY}" && "${USER}" == "${slimline_section_user_host_info_default_user}" ]]; then
+      [[ -z "${SSH_TTY}" && ${UID} -ne 0 ]]; then
     return;
   fi
 
   local -A variables=("user" "%n" "host" "%m")
+  if [[ -z "${SSH_TTY}" && ${UID} -eq 0 ]]; then
+    slimline::utils::expand "user_host_info_root" "%F{red}|user|%f" ${(kv)variables}
+    return;
+  fi
   if [[ ${UID} -eq 0 ]]; then
     slimline::utils::expand "user_host_info_root" "%F{red}|user|%F{white}@%F{yellow}|host|%f" ${(kv)variables}
   else
